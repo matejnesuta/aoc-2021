ad6ffcc12731db589d0da61457c919a864e44533
matejnesuta <matejnetusa@gmail.com>
2022-05-16 23:45:13 +0200

    day 2 task 1

---
+++ day2/task1.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+	// "strconv"
+	"strings"
+)
+
+func perror(msg string) {
+	os.Stderr.WriteString(msg)
+	os.Exit(1)
+}
+
+func main() {
+	var position [2]int
+	position[0] = 0
+	position[1] = 0
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if idx := strings.IndexByte(line, ' '); idx >= 0 {
+			direction := line[:idx]
+			change, err := strconv.Atoi(line[idx+1:])
+			if err != nil {
+				perror("Given position is not an integer. Aborting!")
+			}
+			if direction == "forward" {
+				position[1] += change
+			} else if direction == "up" {
+				position[0] -= change
+			} else if direction == "down" {
+				position[0] += change
+			} else {
+				perror("The given word is not a valid direction. Aborting!")
+			}
+
+		} else {
+			perror("Wrong input format. Aborting the program!")
+		}
+	}
+	fmt.Printf("The final result is: %d\n", position[0]*position[1])
+}
